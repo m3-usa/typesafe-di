6d949975a339e1a1887565fdf7963c6af812dfd7
jooohn <jooohn12341234@gmail.com>
2020-08-03 11:13:03 +0900

    Add helper functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { Design, Injector } from './internal/design';
+export { pick, inject, injectClass } from './internal/helper';
---
+++ src/internal/helper.ts
@@ -0,0 +1,21 @@
+type OptionalKeys<T> = { [K in keyof T]: T[K] extends Required<T>[K] ? never : K }[keyof T];
+type InjectorFor<T, K extends keyof T> = Exclude<keyof T, K | OptionalKeys<T>> extends never
+    ? { [P in K]: Promise<T[P]> }
+    : never;
+
+export async function pick<T, K extends keyof T>(
+    injector: { [P in K]: Promise<T[P]> },
+    keys: K[],
+): Promise<{ [P in K]: T[P] }> {
+    return Object.fromEntries(await Promise.all(keys.map(async name => [name, await injector[name]])));
+}
+
+export const inject = <T, K extends keyof T, V>(f: (params: { [P in K]: T[P] }) => V | Promise<V>, keys: K[]) => async (
+    injector: InjectorFor<T, K>,
+): Promise<V> => {
+    const params = await pick(injector, keys);
+    return f(params);
+};
+
+export const injectClass = <T, K extends keyof T, V>(Class: new (params: { [P in K]: T[P] }) => V, keys: K[]) =>
+    inject((params: { [P in K]: T[P] }) => new Class(params), keys);
